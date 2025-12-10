94d9540330130d0c33ee9ca214546fc6826852a5
김현중 <kimhyunjoong@gimhyeonjung-ui-MacBookPro.local>
2025-12-11 00:29:30 +0900

    Enhance Installer: Add clean uninstall step before installation

--- install_windows.bat
+++ install_windows.bat
@@ -21,7 +21,17 @@ if %errorLevel% == 0 (
     exit /b
 )
 
-echo 1. Installing Plugin (.aex)...
+echo 1. Cleaning up old installation...
+if exist "%PROGRAMFILES%\Adobe\Common\Plug-ins\7.0\MediaCore\AnchorRadialMenu.aex" (
+    echo - Removing old plugin...
+    del /f /q "%PROGRAMFILES%\Adobe\Common\Plug-ins\7.0\MediaCore\AnchorRadialMenu.aex"
+)
+if exist "%PROGRAMFILES(X86)%\Common Files\Adobe\CEP\extensions\com.anchor.grid" (
+    echo - Removing old CEP extension...
+    rmdir /s /q "%PROGRAMFILES(X86)%\Common Files\Adobe\CEP\extensions\com.anchor.grid"
+)
+
+echo 2. Installing Plugin (.aex)...
 if not exist "plugin\AnchorRadialMenu.aex" (
     echo ERROR: Source file 'plugin\AnchorRadialMenu.aex' not found!
     echo Please extract the ZIP file completely before running this script.
@@ -32,7 +42,7 @@ if not exist "plugin\AnchorRadialMenu.aex" (
 mkdir "%PROGRAMFILES%\Adobe\Common\Plug-ins\7.0\MediaCore" 2>nul
 copy /Y plugin\AnchorRadialMenu.aex "%PROGRAMFILES%\Adobe\Common\Plug-ins\7.0\MediaCore\"
 
-echo 2. Installing CEP Panel...
+echo 3. Installing CEP Panel...
 if not exist "cep" (
     echo ERROR: Source folder 'cep' not found!
     pause
@@ -42,7 +52,7 @@ if not exist "cep" (
 mkdir "%PROGRAMFILES(X86)%\Common Files\Adobe\CEP\extensions\com.anchor.grid" 2>nul
 xcopy /E /I /Y cep "%PROGRAMFILES(X86)%\Common Files\Adobe\CEP\extensions\com.anchor.grid"
 
-echo 3. Enabling Debug Mode (Registry)...
+echo 4. Enabling Debug Mode (Registry)...
 reg add "HKCU\Software\Adobe\CSXS.10" /v PlayerDebugMode /t REG_SZ /d 1 /f >nul
 reg add "HKCU\Software\Adobe\CSXS.11" /v PlayerDebugMode /t REG_SZ /d 1 /f >nul
 reg add "HKCU\Software\Adobe\CSXS.12" /v PlayerDebugMode /t REG_SZ /d 1 /f >nul
